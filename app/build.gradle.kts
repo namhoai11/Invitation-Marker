750763f6ac8768247b6272fe97c4c81d4fd78068
LaHoaiNam <20521629@gm.uit.edu.vn>
2025-06-03 11:58:24 +0700

    CardEditDetailsUI_Layout, Add Dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.wuapnjie:StickerView:4a82f82fba")
 }
